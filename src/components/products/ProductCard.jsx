717c5dcee23713ea194930b9fadeb70ccef7c728
Square <aayushk4980@gmail.com>
2024-07-20 01:48:09 +0530

    Added ProductCard component

---
+++ src/components/products/ProductCard.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function ProductCard({ productDetails }) {
+
+    // Getting product details.
+    const productImg = productDetails['image'];
+    const productName = productDetails['name'];
+    const originalPrice = productDetails['originalPrice'];
+    const discountPercentage = productDetails['discountPercentage'];
+
+    return (
+        <div className="relative p-4 my-4 mx-auto w-64 max-w-[70vw] rounded-2xl shadow-product-card-shadow hover:shadow-product-card-shadow-hover duration-300">
+
+            <button className="absolute top-0 right-0 m-2">
+                <FontAwesomeIcon icon="fa-solid fa-heart" className="h-5 text-slate-400" />
+            </button>
+
+            <img src={productImg} alt="" className="block m-auto h-60" />
+
+            <div className="flex flex-row items-start justify-between my-4">
+                <p className="font-semibold text-slate-900">
+                    {productName}
+                </p>
+
+                <div className="bg-slate-900 p-1 text-white flex flex-row items-start justify-center gap-1 rounded-lg">
+                    4
+                    <FontAwesomeIcon icon="fa-solid fa-star" className="h-3" />
+                </div>
+            </div>
+
+            <div className="flex flex-row items-center justify-between mt-8">
+                <p>
+                    <span className="font-semibold text-slate-900">
+                        &#8377;
+                        {
+                            originalPrice - originalPrice * (discountPercentage / 100)
+                        }
+                    </span>
+                    <span className="ml-2 line-through text-slate-500 font-thin">
+                        &#8377;{originalPrice}
+                    </span>
+                </p>
+                <p className="font-semibold text-green-600">{discountPercentage}% OFF</p>
+            </div>
+
+            <button className="bg-slate-900 text-white w-full mt-4 rounded-md py-[6px] tracking-wider uppercase">
+                Add To Cart
+            </button>
+        </div>
+    );
+};
