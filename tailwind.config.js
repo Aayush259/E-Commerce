f9ca2556c06f60965eacf4bdb88d2d2581352cd0
Square <aayushk4980@gmail.com>
2024-07-19 19:55:18 +0530

    Added Loader component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'loading': {
+          '0%': { transform: 'scale(1)', opacity: '1' },
+          '50%': { transform: 'scale(0)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '1' }
+        }
+      },
+
+      animation: {
+        'load-1': 'loading 1s ease-in-out infinite',
+        'load-2': 'loading 1s ease-in-out 0.2s infinite',
+        'load-3': 'loading 1s ease-in-out 0.4s infinite'
+      }
+    },
   },
   plugins: [],
 }
