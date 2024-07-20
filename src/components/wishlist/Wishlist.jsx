87388e9e89efd6b2e98ac00990878d1ff66c6c4b
Square <aayushk4980@gmail.com>
2024-07-20 15:42:57 +0530

    Fixed Wishlist styling

--- src/components/wishlist/Wishlist.jsx
+++ src/components/wishlist/Wishlist.jsx
@@ -10,7 +10,7 @@ export default function Wishlist() {
         <div className="flex flex-row flex-wrap justify-start items-start gap-4 w-fit my-8 mx-4">
             {
                 wishlistItems.length <= 0 ? (
-                    <p className="text-xl w-[100vw] md:text-3xl font-semibold tracking-wider text-center my-10">
+                    <p className="absolute top-20 left-0 text-xl w-[100vw] overflow-hidden md:text-3xl font-semibold tracking-wider text-center my-10">
                         You haven't added anything in the wishlist yet.
                     </p>
                 ) : (
