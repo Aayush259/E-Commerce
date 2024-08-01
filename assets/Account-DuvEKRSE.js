f8ccbb6bb704e0f6e52b1f563feddbcd60da6635
Aayush259 <Aayush259@users.noreply.github.com>
2024-08-01 13:08:42 +0000

    deploy: 1681913a706542cc4b3c4dacaa3b46c4f04b9bdb

--- assets/Account-DuvEKRSE.js
+++ assets/Account-DuvEKRSE.js
@@ -1,4 +1,4 @@
-import{j as e,N as a,P as i,r as l,b as o,O as n}from"./index-DE4zhR5T.js";function s({linkTo:t}){return e.jsx(a,{to:t,className:({isActive:r})=>`
+import{j as e,N as r,P as i,r as l,a as o,O as n}from"./index-BnsFymND.js";function s({linkTo:t}){return e.jsx(r,{to:t,className:({isActive:a})=>`
         flex-grow py-3 px-2 md:text-xl text-center capitalize
-        ${r?"bg-slate-900 text-white":"bg-white text-slate-900"}
+        ${a?"bg-slate-900 text-white":"bg-white text-slate-900"}
       `,children:t})}s.propTypes={linkTo:i.string.isRequired};function c(){return e.jsxs("div",{className:"w-[90vw] max-w-[600px] m-auto my-6 shadow-product-card-shadow rounded-b-2xl overflow-hidden",children:[e.jsxs("div",{className:"w-full flex flex-row items-center justify-center",children:[e.jsx(s,{linkTo:"profile"}),e.jsx(s,{linkTo:"address"})]}),e.jsx(l.Suspense,{fallback:e.jsx(o,{}),children:e.jsx(n,{})})]})}export{c as default};
