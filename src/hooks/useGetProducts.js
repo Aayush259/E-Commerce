25d814c808b839678778134153d5f98244e5a61c
Square <aayushk4980@gmail.com>
2024-08-02 16:25:28 +0530

    Custom hook to dispatch action to fetch products

---
+++ src/hooks/useGetProducts.js
@@ -0,0 +1,16 @@
+import { useDispatch } from 'react-redux';
+import { getProducts } from '../features/product/productSlice.js';
+import { useProducts } from './useStoreItems.js';
+
+const useGetProducts = () => {
+
+    const dispatch = useDispatch();
+    const productData = useProducts();
+
+    if (productData && productData.length > 0) return;
+
+
+    dispatch(getProducts());
+};
+
+export default useGetProducts;
