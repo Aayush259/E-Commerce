42f8324284183ff6931fb4182dffc9dd3e91b8ac
Aayush <aayushk4980@gmail.com>
2024-11-02 23:13:12 +0530

    Signup done

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { signup } from '../app/auth';
 
 export default function Signup() {
 
+    const navigate = useNavigate();
+
+    const [data, setData] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+
+    const handleSignup = async (e) => {
+        e.preventDefault();
+
+        if (!data.name && !data.email && !data.password) return alert('All fields are required.');
+
+        const res = await signup(data.name, data.email, data.password);
+
+        console.log(res);
+
+        if (res.ok) {
+            navigate('/E-Commerce/login');
+        }
+    };
+
     return (
         <div className="flex min-h-[100vh] w-[100vw] max-w-[600px] m-auto flex-col items-center justify-center px-7">
-            <form className="w-full">
+            <form className="w-full" onSubmit={handleSignup}>
                 <h2 className="text-center font-semibold text-2xl md:text-3xl mb-4">Sign Up</h2>
 
                 {
-                    ['email', 'password'].map((type) => (
+                    ['name', 'email', 'password'].map((type) => (
                         <label key={type} htmlFor={type} className="w-full my-4 text-slate-800 flex flex-col gap-2">
                             {type.charAt(0).toUpperCase() + type.slice(1)}
                             <input
@@ -17,6 +40,9 @@ export default function Signup() {
                                 name={type}
                                 id={type}
                                 className="bg-red-100 p-2 w-full border border-slate-900 rounded-md"
+                                value={data[type]}
+                                onChange={(e) => setData({ ...data, [type]: e.target.value })}
+                                required
                             />
                         </label>
                     ))
@@ -24,7 +50,6 @@ export default function Signup() {
 
                 <button
                     className={`w-full bg-slate-900 text-white my-3 text-lg md:text-xl py-2 border border-slate-900 hover:bg-white hover:text-slate-900 mt-4 duration-300`}
-                    onClick={(e) => e.preventDefault()}
                 >
                     Sign Up
                 </button>
