2c314b6f44a7f1725c3e4a4ffae76f3737789f13
Aayush <aayushk4980@gmail.com>
2024-11-02 23:22:24 +0530

    Login Done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { login } from '../app/auth';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../features/user/userSlice';
 
 export default function Login() {
 
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const [data, setData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        if (!data.email && !data.password) return alert('All fields are required.');
+
+        const res = await login(data.email, data.password);
+
+        if (res.ok) {
+            navigate('/E-Commerce/products');
+            dispatch(setUser({ isLoggedIn: true, user: res.data.user }));
+        }
+    };
+
     return (
         <div className="flex min-h-[100vh] w-[100vw] max-w-[600px] m-auto flex-col items-center justify-center px-7">
-            <form className="w-full">
+            <form className="w-full" onSubmit={handleLogin}>
                 <h2 className="text-center font-semibold text-2xl md:text-3xl mb-4">Login</h2>
 
                 {
@@ -16,6 +40,9 @@ export default function Login() {
                                 type={type}
                                 name={type}
                                 id={type}
+                                value={data[type]}
+                                onChange={(e) => setData({ ...data, [type]: e.target.value })}
+                                required
                                 className="bg-red-100 p-2 w-full border border-slate-900 rounded-md"
                             />
                         </label>
@@ -23,8 +50,8 @@ export default function Login() {
                 }
 
                 <button
+                    type='submit'
                     className={`w-full bg-slate-900 text-white my-3 text-lg md:text-xl py-2 border border-slate-900 hover:bg-white hover:text-slate-900 mt-4 duration-300`}
-                    onClick={(e) => e.preventDefault()}
                 >
                     Login
                 </button>
