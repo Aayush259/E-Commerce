6023690157c04ffe777b99ab82156f59f70d4892
Aayush <aayushk4980@gmail.com>
2024-11-10 21:16:15 +0530

    Update useStoreItem.js

--- src/hooks/useStoreItems.js
+++ src/hooks/useStoreItems.js
@@ -6,24 +6,30 @@ const useUser = () => useSelector(state => state.user);
 // Custom hook to use product state from store.
 const useProducts = () => useSelector(state => state.product.value);
 
-// Custom hook to use cart state from store.
-const useCartItems = () => useSelector(state => state.cart.value);
-
-// Custom hook to use wishlist state from store.
-const useWishlistItems = () => useSelector(state => state.wishlist.value);
-
 // Custom hook to know whether the item is already in the cart state or not.
-const useIsItemInCart = () => (itemName) => {
-    const cartItems = useCartItems();
-    
-    return cartItems.some(item => item.name === itemName);
+const useIsItemInCart = () => (id) => {
+    // const cartItems = useCartItems();
+
+    // return cartItems.some(item => item.name === itemName);
+
+    const { user, isLoggedIn } = useUser();
+    if (!isLoggedIn) {
+        return false;
+    }
+    return user.cart.some(cartProdId => cartProdId === id);
 };
 
 // Custom hook to know whether the item is already in the wishlist state or not.
-const useIsItemInWishlist = () => (itemName) => {
-    const wishlistItems = useWishlistItems();
+const useIsItemInWishlist = () => (id) => {
+    // const wishlistItems = useWishlistItems();
 
-    return wishlistItems.some(item => item.name === itemName);
+    // return wishlistItems.some(item => item.name === itemName);
+
+    const { user, isLoggedIn } = useUser();
+    if (!isLoggedIn) {
+        return false;
+    }
+    return user.wishlist.some(wishlistProdId => wishlistProdId === id);
 };
 
-export { useUser, useProducts, useCartItems, useWishlistItems, useIsItemInCart, useIsItemInWishlist };
+export { useUser, useProducts, useIsItemInCart, useIsItemInWishlist };
