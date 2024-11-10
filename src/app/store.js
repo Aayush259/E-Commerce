b22370d8e3b2a1cc16fba743685d060ad8418baf
Aayush <aayushk4980@gmail.com>
2024-11-10 21:16:54 +0530

    Updated store

--- src/app/store.js
+++ src/app/store.js
@@ -1,18 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../features/user/userSlice.js';
-import cartSlice from '../features/cart/cartSlice.js';
-import wishlistSlice from '../features/wishlist/wishlistSlice.js';
 import productSlice from '../features/product/productSlice.js';
-import { cartLocalStorageMiddleWare, wishlistLocalStorageMiddleWare } from './localStorageMiddleWare.js';
 
 const store = configureStore({
     reducer: {
         user: userSlice,
         product: productSlice,
-        cart: cartSlice,
-        wishlist: wishlistSlice,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([cartLocalStorageMiddleWare(), wishlistLocalStorageMiddleWare()]),
 });
 
 export default store;
