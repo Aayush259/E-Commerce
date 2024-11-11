82a4765ce6c59ab59fc834868c4ab3a6d552bbe7
Aayush <aayushk4980@gmail.com>
2024-11-11 20:28:56 +0530

    Update Search.jsx

--- src/components/nav/Search.jsx
+++ src/components/nav/Search.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useNavigate } from 'react-router-dom';
 
-export default function Search() {
+export default function Search({ setSearchOpen }) {
 
     // Reference for input element.
     const inputRef = useRef();
@@ -12,6 +12,7 @@ export default function Search() {
 
     // This function handles the click on search button.
     const handleSearchBtnClick = () => {
+        setSearchOpen(false);
         if (inputRef.current.value) {
             navigate(`/E-Commerce/products/productName/${inputRef.current.value}`);
         }
@@ -25,7 +26,7 @@ export default function Search() {
     };
 
     return (
-        <div className="bg-white max-w-[90%] mx-auto my-5 rounded-3xl text-black font-normal overflow-hidden flex flex-row items-center justify-center">
+        <div className="bg-white mx-auto my-5 rounded-3xl text-black font-normal overflow-hidden flex flex-row items-center justify-center">
             <label htmlFor="searchBar">
                 <input
                     ref={inputRef}
