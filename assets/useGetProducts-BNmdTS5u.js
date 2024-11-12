b3f6451c68362fc947aac6906ecbb2a0e20c0465
Aayush259 <Aayush259@users.noreply.github.com>
2024-11-12 15:34:36 +0000

    deploy: a26c84647d51199a3154be1a1314246785845d3a

--- assets/useGetProducts-BNmdTS5u.js
+++
@@ -1,1 +0,0 @@
-import{c,s as o,t as r}from"./index-DwGg0Xxk.js";const a=()=>{const s=c(),t=o();t&&t.length>0||s(r())};export{a as u};
---
+++ assets/useGetProducts-ClGwtbQ3.js
@@ -0,0 +1,1 @@
+import{c,s as r,r as e,t as o}from"./index-CHYNfWYa.js";const u=()=>{const s=c(),t=r();t&&t.length>0||e.useEffect(()=>{s(o())},[])};export{u};
