f7966105a3b3dcf951f199b14a1146c99593ff06
Aayush259 <Aayush259@users.noreply.github.com>
2024-11-14 17:13:35 +0000

    deploy: 78f12e072dd7729e16191525a3a7162b0ebe31c5

--- assets/Account-CEvVIlR5.js
+++ assets/Account-CEvVIlR5.js
@@ -1,4 +1,4 @@
-import{j as e,N as a,P as i,r as l,s as o,O as n}from"./index-DdsIW3CV.js";function s({linkTo:t}){return e.jsx(a,{to:t,className:({isActive:r})=>`
+import{j as e,N as a,P as i,r as l,s as o,O as n}from"./index-KYigPCpy.js";function s({linkTo:t}){return e.jsx(a,{to:t,className:({isActive:r})=>`
         flex-grow py-3 px-2 md:text-xl text-center capitalize
         ${r?"bg-slate-900 text-white":"bg-white text-slate-900"}
       `,children:t})}s.propTypes={linkTo:i.string.isRequired};function c(){return e.jsxs("div",{className:"w-[90vw] max-w-[600px] m-auto my-6 shadow-product-card-shadow rounded-b-2xl overflow-hidden",children:[e.jsxs("div",{className:"w-full flex flex-row items-center justify-center",children:[e.jsx(s,{linkTo:"profile"}),e.jsx(s,{linkTo:"address"})]}),e.jsx(l.Suspense,{fallback:e.jsx(o,{}),children:e.jsx(n,{})})]})}export{c as default};
